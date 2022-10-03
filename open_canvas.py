dd11443fd8642f8885b3592a6133465c961cdf52
mypuhaba <mypuhaba@gmail.com>
2022-10-03 22:46:33 +0900

    Collect Resourses & Test

---
+++ open_canvas.py
@@ -0,0 +1,16 @@
+from pico2d import *
+open_canvas()
+gunner = load_image('male_gunner_sprite\\walk.png')
+x = 70
+n = 0
+frame = 0
+while (x < 800):
+    clear_canvas() # width, height = 205, 173
+    gunner.clip_draw(frame * 205, 0, 205, 173, x, 80)
+    update_canvas()
+    frame = (frame + 1) % 4
+    x += 10
+    delay(0.1)
+    get_events()
+
+close_canvas()
