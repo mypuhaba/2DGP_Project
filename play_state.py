8ee7bf67c629f303ca0440ff69a9f67311d67dd0
Park seong ro <mypuhaba@gmail.com>
2022-10-18 23:48:27 +0900

    플레이어 공격 모션 추가

--- play_state.py
+++ play_state.py
@@ -1,6 +1,8 @@
+import pico2d
 from pico2d import *
 import game_framework
 import play_state
+
 
 class Forest:
     def __init__(self):
@@ -11,6 +13,7 @@ class Forest:
 
     def draw(self):
         self.image.draw(400, 300)
+
 
 class Grass:
     def __init__(self):
@@ -24,28 +27,40 @@ class Gunner:
     def __init__(self):
         self.x, self.y = 0, 125
         self.frame = 0
-        self.dir = 1
+        self.dir, self.face_dir = 0, 1
+        self.shot = 0
         self.normalR = load_image('gunner_sprite/normal.png')
-        # self.normalL = load_image('gunner_sprite/normal.png')
+        self.normalL = load_image('gunner_sprite/normal_left.png')
         self.runR = load_image('gunner_sprite/run.png')
         self.runL = load_image('gunner_sprite/run_left.png')
         # self.jumpR = load_image('gunner_sprite/jump.png')
         # self.jumpL = load_image('gunner_sprite/jump_left.png')
+        self.shotR = load_image('gunner_sprite/shot.png')
+        self.shotL = load_image('gunner_sprite/shot_left.png')
 
     def update(self):
         self.frame = (self.frame + 1) % 8
-        # self.x += self.dir * 10
-        if self.x > 800:
-            self.x = 800
-        elif self.x < 0:
-            self.x = 0
-        delay(0.1)
+        self.x += self.dir * 10
+        self.x = clamp(40, self.x, 800)
+        delay(0.05)
 
     def draw(self):
         if self.dir == 1:
             self.runR.clip_draw(self.frame * 205, 0, 205, 173, self.x, self.y)
-        else:
+        elif self.dir == -1:
             self.runL.clip_draw(self.frame * 205, 0, 205, 173, self.x, self.y)
+        else:
+            if self.face_dir == 1:
+                if self.shot == 1:
+                    self.shotR.clip_draw((self.frame * 7 // 8) * 205, 0, 205, 173, self.x, self.y)
+                else:
+                    self.normalR.clip_draw(self.frame // 2 * 205, 0, 205, 173, self.x, self.y)
+            else:
+                if self.shot == 1:
+                    self.shotL.clip_draw((self.frame * 7 // 8) * 205, 0, 205, 173, self.x, self.y)
+                else:
+                    self.normalL.clip_draw(self.frame // 2 * 205, 0, 205, 173, self.x, self.y)
+
 
 
 def handle_events():
@@ -53,18 +68,27 @@ def handle_events():
     for event in events:
         if event.type == pico2d.SDL_QUIT:
             game_framework.quit()
-        elif event.type == pico2d.SDL_KEYDOWN:
+        elif event.type == SDL_KEYDOWN:
             match event.key:
                 case pico2d.SDLK_ESCAPE:
                     game_framework.quit()
-                # case pico2d.SDLK_RIGHT():
-                #     play_state.gunner.dir = 1
-                # case pico2d.SDLK_LEFT():
-                #     play_state.gunner.dir = -1
-                # case pico2d.SDLK_x:
+                case pico2d.SDLK_LEFT:
+                    gunner.dir -= 1
+                case pico2d.SDLK_RIGHT:
+                    gunner.dir += 1
+                case pico2d.SDLK_x:
+                    gunner.shot = 1
 
-
-
+        elif event.type == SDL_KEYUP:
+            match event.key:
+                case pico2d.SDLK_LEFT:
+                    gunner.dir += 1
+                    gunner.face_dir = -1
+                case pico2d.SDLK_RIGHT:
+                    gunner.dir -= 1
+                    gunner.face_dir = 1
+                case pico2d.SDLK_x:
+                    gunner.shot = 0
 
 
 forest = None
@@ -121,5 +145,5 @@ def test_self():
     pico2d.close_canvas()
 
 
-if __name__ == '__main__': #만약 단독 실행 상태이면,
+if __name__ == '__main__':  # 만약 단독 실행 상태이면,
     test_self()
